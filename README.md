5c9bba1243f4a864c67865e4565f2ee1aaaa332c
Brainforge.UK <brainforge.uk@btinternet.com>
2022-11-24 18:12:07 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+This plugin allows you to insert a Paypal button into an article / other content or module by using:
+``{bfpaypalbutton}``
+
+You can add a suggested amount:
+``{bfpaypalbutton Suggest £5}``
+
+For more information see:
+<ul>
+<li>https://www.paypal.com/buttons/</li>
+<li>https://www.paypal.com/donate/buttons?type=C&fromManage=true</li>
+</ul>
---
+++ bfpaypalbutton.php
@@ -0,0 +1,56 @@
+<?php
+/**
+* @package   PayPal Button
+* @version   0.0.1
+* @author    http://www.brainforge.co.uk
+* @copyright Copyright (C) 2022 Jonathan Brain. All rights reserved.
+* @license	 GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.plugin.plugin');
+
+class plgContentBFPaypalButton extends JPlugin
+{
+	protected $app;
+	protected $hasButton = false;
+
+	/**
+	*/
+	public function __construct(&$subject, $config = array())
+	{
+		parent::__construct($subject, $config);
+	}
+
+	/**
+	*
+	*/
+	public function onContentPrepare($context, &$article, &$params, $limitstart)
+	{
+		$buttoncode = trim($this->params->get('buttoncode'));
+
+		if (empty($buttoncode)) return;
+
+		if (!$this->app->isClient('site')) return;
+
+		if (preg_match_all('/{bfpaypalbutton}/i', $article->text, $matches, PREG_SET_ORDER))
+		{
+			foreach ($matches as $match)
+			{
+				if ($this->hasButton)
+				{
+					$article->text = str_replace($matches[0], '', $article->text);
+					continue;
+				}
+
+				// Hides button in module when it is already in page content
+				$this->hasButton = true;
+
+				$article->text = str_replace('{bfpaypalbutton}', $buttoncode, $article->text);
+			}
+		}
+	}
+}
+?>
